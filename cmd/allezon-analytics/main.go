f08ce08000e984f2f5420448d4a9c01ba8ce2f14
Kacper Rzetelski <rzetelski.k@gmail.com>
2022-07-05 14:48:08 +0200

    Implement monolith in memory solution for use cases 1 and 2

--- cmd/allezon-analytics/main.go
+++ cmd/allezon-analytics/main.go
@@ -3,22 +3,123 @@ package main
 import (
 	"encoding/json"
 	"flag"
-	"io"
-	"net/http"
-
+	"fmt"
 	"github.com/gorilla/mux"
+	"io"
 	"k8s.io/klog/v2"
+	"net/http"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
 )
 
+var views = MapStore{
+	m: make(map[string][]UserTag),
+}
+
+var buys = MapStore{
+	m: make(map[string][]UserTag),
+}
+
+type Device int
+
+const (
+	PC Device = iota
+	MOBILE
+	TV
+)
+
+func (d Device) MarshalJSON() ([]byte, error) {
+	var s string
+
+	switch d {
+	case PC:
+		s = `"PC"`
+	case MOBILE:
+		s = `"MOBILE"`
+	case TV:
+		s = `"TV"`
+	default:
+		return nil, fmt.Errorf("device is not supported")
+	}
+
+	return []byte(s), nil
+}
+
+func (d *Device) UnmarshalJSON(data []byte) error {
+	var err error
+	var s string
+
+	err = json.Unmarshal(data, &s)
+	if err != nil {
+		return fmt.Errorf("can't unmarshal to string: %w", err)
+	}
+
+	switch s {
+	case "PC":
+		*d = PC
+	case "MOBILE":
+		*d = MOBILE
+	case "TV":
+		*d = TV
+	default:
+		return fmt.Errorf("device '%s' is invalid", s)
+	}
+
+	return nil
+}
+
+type Action int
+
+const (
+	VIEW Action = iota
+	BUY
+)
+
+func (a Action) MarshalJSON() ([]byte, error) {
+	var s string
+
+	switch a {
+	case VIEW:
+		s = `"VIEW"`
+	case BUY:
+		s = `"BUY"`
+	default:
+		return nil, fmt.Errorf("action is not supported")
+	}
+
+	return []byte(s), nil
+}
+
+func (a *Action) UnmarshalJSON(data []byte) error {
+	var err error
+	var s string
+
+	err = json.Unmarshal(data, &s)
+	if err != nil {
+		return fmt.Errorf("can't unmarshal to string: %w", err)
+	}
+
+	switch s {
+	case "VIEW":
+		*a = VIEW
+	case "BUY":
+		*a = BUY
+	default:
+		return fmt.Errorf("action '%s' is invalid", s)
+	}
+
+	return nil
+}
+
 type UserTag struct {
-	Time string `json:"time"` // format: "2022-03-22T12:15:00.000Z"
-	//   millisecond precision
-	//   with 'Z' suffix
-	Cookie      string `json:"cookie"`
-	Country     string `json:"country"`
-	Device      string `json:"device"` // FIXME: enum
-	Action      string `json:"action"` // FIXME: enum
-	Origin      string `json:"origin"`
+	Time        time.Time `json:"time"`
+	Cookie      string    `json:"cookie"`
+	Country     string    `json:"country"`
+	Device      Device    `json:"device"`
+	Action      Action    `json:"action"`
+	Origin      string    `json:"origin"`
 	ProductInfo struct {
 		ProductID  uint64 `json:"product_id"` // FIXME: it's supposed to be a string
 		BrandID    string `json:"brand_id"`
@@ -27,12 +128,47 @@ type UserTag struct {
 	} `json:"product_info"`
 }
 
+const (
+	RFC3339Milli = "2006-01-02T15:04:05.000Z"
+	UTC          = "2006-01-02T15:04:05"
+	UTCMilli     = "2006-01-02T15:04:05.000"
+)
+
+func (ut *UserTag) UnmarshalJSON(data []byte) error {
+	var err error
+
+	type Alias UserTag
+	aux := &struct {
+		Time string `json:"time"`
+		*Alias
+	}{
+		Alias: (*Alias)(ut),
+	}
+
+	err = json.Unmarshal(data, &aux)
+	if err != nil {
+		return err
+	}
+
+	for _, f := range []string{RFC3339Milli, "2006-01-02T15:04:05Z"} { // FIXME: remove this after email
+		ut.Time, err = time.Parse(f, aux.Time)
+		if err == nil {
+			break
+		}
+	}
+	if err != nil {
+		return fmt.Errorf("can't parse time: %w", err)
+	}
+
+	return nil
+}
+
 func UserTagsPostHandler(w http.ResponseWriter, r *http.Request) {
 	data, err := io.ReadAll(r.Body)
 	defer r.Body.Close()
 
 	if err != nil {
-		klog.Errorf("can't read io: %w", err)
+		klog.Errorf("can't read io: %v", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -40,7 +176,22 @@ func UserTagsPostHandler(w http.ResponseWriter, r *http.Request) {
 	ut := UserTag{}
 	err = json.Unmarshal(data, &ut)
 	if err != nil {
-		klog.Errorf("can't unmarshall data: %w", err)
+		klog.Errorf("can't unmarshall data: %v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var m *MapStore
+	switch ut.Action {
+	case VIEW:
+		m = &views
+	case BUY:
+		m = &buys
+	}
+
+	err = m.Append(ut.Cookie, ut)
+	if err != nil {
+		klog.Errorf("can't append data: %v", err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
@@ -54,14 +205,124 @@ type UserProfileResponse struct {
 	Buys   []UserTag `json:"buys"`
 }
 
-func UserProfilesPostHandler(w http.ResponseWriter, r *http.Request) {
-	data, err := io.ReadAll(r.Body)
-	defer r.Body.Close()
+func ParseDateTimeSeconds(s string) (time.Time, error) {
+	return time.Parse(UTC, s)
+}
 
+func ParseDatetimeMilliseconds(s string) (time.Time, error) {
+	return time.Parse(UTCMilli, s)
+}
+
+func ParseTimeRange(s string, datetimeParseFunc func(string) (time.Time, error)) (time.Time, time.Time, error) {
+	var err error
+
+	trs := strings.Split(s, "_")
+	if len(trs) != 2 {
+		return time.Time{}, time.Time{}, fmt.Errorf("invalid time range format")
+	}
+
+	lowerBound, err := datetimeParseFunc(trs[0])
 	if err != nil {
-		klog.Errorf("can't read io: %w", err)
+		return time.Time{}, time.Time{}, fmt.Errorf("invalid lower bound")
+	}
+
+	upperBound, err := datetimeParseFunc(trs[1])
+	if err != nil {
+		return time.Time{}, time.Time{}, fmt.Errorf("invalid upper bound")
+	}
+
+	return lowerBound, upperBound, nil
+}
+
+func UserProfilesPostHandler(w http.ResponseWriter, r *http.Request) {
+	var err error
+
+	cookie := mux.Vars(r)["cookie"]
+
+	values := r.URL.Query()
+
+	if !values.Has("time_range") {
+		http.Error(w, "required parameter 'time_range' is missing", http.StatusBadRequest)
+		return
+	}
+
+	timeRange := values.Get("time_range")
+	lowerBound, upperBound, err := ParseTimeRange(timeRange, ParseDatetimeMilliseconds)
+	if err != nil {
+		http.Error(w, fmt.Errorf("can't parse time range: %w", err).Error(), http.StatusBadRequest)
+		return
+	}
+
+	var limit int
+	if values.Has("limit") {
+		limit, err = strconv.Atoi(values.Get("limit"))
+		if err != nil {
+			http.Error(w, "optional parameter 'limit' is invalid", http.StatusBadRequest)
+			return
+		}
+	} else {
+		limit = 200
+	}
+
+	/* GET VIEWS */
+	vs, err := views.Get(cookie)
+	if err != nil {
+		klog.Errorf("can't get views for cookie %s: %v", cookie, err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
+	}
+
+	//Filter
+	vsTmp := make([]UserTag, 0)
+	for _, v := range vs {
+		if (v.Time.After(lowerBound) || v.Time.Equal(lowerBound)) && v.Time.Before(upperBound) {
+			vsTmp = append(vsTmp, v)
+		}
+	}
+	vs = vsTmp
+
+	// Sort
+	sort.Slice(vs, func(i, j int) bool {
+		return vs[i].Time.After(vs[j].Time)
+	})
+
+	// Limit
+	vs = vs[max(0, len(vs)-limit):]
+
+	/* GET BUYS */
+	bs, err := buys.Get(cookie)
+	if err != nil {
+		klog.Errorf("can't get buys for cookie %s: %v", cookie, err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Filter
+	bsTmp := make([]UserTag, 0)
+	for _, b := range bs {
+		if (b.Time.After(lowerBound) || b.Time.Equal(lowerBound)) && b.Time.Before(upperBound) {
+			bsTmp = append(bsTmp, b)
+		}
+	}
+	bs = bsTmp
+
+	// Sort
+	sort.Slice(bs, func(i, j int) bool {
+		return bs[i].Time.After(bs[j].Time)
+	})
+
+	// Limit
+	bs = bs[max(0, len(bs)-limit):]
+
+	res := UserProfileResponse{
+		Cookie: cookie,
+		Views:  vs,
+		Buys:   bs,
+	}
+
+	data, err := json.Marshal(res)
+	if err != nil {
+		klog.Errorf("can't marshal data: %v", err)
 	}
 
 	w.Header().Set("Content-Type", "application/json")
@@ -73,7 +334,7 @@ func AggregatesPostHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	if err != nil {
-		klog.Errorf("can't read io: %w", err)
+		klog.Errorf("can't read io: %v", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
---
+++ cmd/allezon-analytics/store.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"golang.org/x/exp/constraints"
+	"sync"
+)
+
+type MapStore struct {
+	sync.RWMutex
+	m map[string][]UserTag
+}
+
+func max[T constraints.Ordered](a, b T) T {
+	if a < b {
+		return b
+	}
+	return a
+}
+
+func (s *MapStore) Append(key string, value UserTag) error {
+	s.Lock()
+	arr := s.m[key]
+	s.m[key] = append(arr[max(len(arr)-199, 0):], value)
+	s.Unlock()
+
+	return nil
+}
+
+func (s *MapStore) Get(key string) ([]UserTag, error) {
+	s.RLock()
+	v, ok := s.m[key]
+	s.RUnlock()
+
+	if !ok {
+		v = make([]UserTag, 0)
+	}
+
+	return v, nil
+}
